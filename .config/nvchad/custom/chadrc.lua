9515a78c5c61336d9e228f7549d3f9ade57f8d80
Tian Pengfei <itsneo1990@gmail.com>
2022-09-13 10:41:23 +0800

    update nvchad

--- .config/nvchad/custom/chadrc.lua
+++ .config/nvchad/custom/chadrc.lua
@@ -11,102 +11,89 @@ M.ui = {
 }
 
 M.plugins = {
-	options = {
-		lspconfig = {
-			setup_lspconf = "custom.override.lspconfig",
-		},
+	["williamboman/mason.nvim"] = {
+		override_options = require("custom.override.mason"),
 	},
-	override = {
-		["williamboman/mason.nvim"] = require("custom.override.mason"),
-		["NvChad/nvterm"] = require("custom.override.nvterm"),
-		["hrsh7th/nvim-cmp"] = require("custom.override.nvim-cmp"),
-		["kyazdani42/nvim-tree.lua"] = require("custom.override.nvim-tree"),
+	["NvChad/nvterm"] = {
+		override_options = require("custom.override.nvterm"),
 	},
-	user = {
-		["goolord/alpha-nvim"] = { disable = false },
-		["glepnir/lspsaga.nvim"] = {
-			config = function()
-				local saga = require("lspsaga")
-				saga.init_lsp_saga()
-				vim.diagnostic.config({ virtual_text = false })
-			end,
-		},
-		["kylechui/nvim-surround"] = {
-			config = function()
-				require("nvim-surround").setup({})
-			end,
-		},
-		["RRethy/vim-illuminate"] = {
-			config = function()
-				require("illuminate").configure({
-					-- providers: provider used to get references in the buffer, ordered by priority
-					providers = {
-						"lsp",
-						"treesitter",
-						"regex",
-					},
-					filetypes_denylist = {
-						"NvimTree",
-						"dirvish",
-						"fugitive",
-					},
-					-- delay: delay in milliseconds
-					delay = 100,
-				})
-			end,
-		},
-		["neovim/nvim-lspconfig"] = {
-			config = function()
-				require("plugins.configs.lspconfig")
-				require("custom.override.lspconfig")
-			end,
-		},
-		["jose-elias-alvarez/null-ls.nvim"] = {
-			after = "nvim-lspconfig",
-			config = function()
-				require("custom.plugins.null_ls").setup()
-			end,
-		},
-		["folke/todo-comments.nvim"] = {
-			config = function()
-				require("todo-comments").setup()
-			end,
-			event = "BufRead",
-		},
-		["simrat39/symbols-outline.nvim"] = {
-			event = "BufReadPost",
-			config = function()
-				require("symbols-outline").setup()
-			end,
-		},
-		["j-hui/fidget.nvim"] = {
-			config = function()
-				require("fidget").setup({})
-			end,
-		},
-		["b0o/schemastore.nvim"] = {},
-		["williamboman/mason.nvim"] = {
-			ensure_installed = {
-				-- lua stuff
-				"lua-language-server",
-				"stylua",
-				-- web dev
-				"json-lsp",
-				-- shell
-				"shfmt",
-				"shellcheck",
-			},
-		},
-		["ahmedkhalf/project.nvim"] = {
-			config = function()
-				require("project_nvim").setup()
-			end,
-		},
-		["andrewferrier/debugprint.nvim"] = {
-			config = function()
-				require("debugprint").setup()
-			end,
-		},
+	["hrsh7th/nvim-cmp"] = {
+		override_options = require("custom.override.nvim-cmp"),
+	},
+	["kyazdani42/nvim-tree.lua"] = {
+		override_options = require("custom.override.nvim-tree"),
+	},
+	["goolord/alpha-nvim"] = { disable = false },
+	["glepnir/lspsaga.nvim"] = {
+		config = function()
+			local saga = require("lspsaga")
+			saga.init_lsp_saga()
+			vim.diagnostic.config({ virtual_text = false })
+		end,
+	},
+	["kylechui/nvim-surround"] = {
+		config = function()
+			require("nvim-surround").setup({})
+		end,
+	},
+	["RRethy/vim-illuminate"] = {
+		config = function()
+			require("illuminate").configure({
+				-- providers: provider used to get references in the buffer, ordered by priority
+				providers = {
+					"lsp",
+					"treesitter",
+					"regex",
+				},
+				filetypes_denylist = {
+					"NvimTree",
+					"dirvish",
+					"fugitive",
+				},
+				-- delay: delay in milliseconds
+				delay = 100,
+			})
+		end,
+	},
+	["neovim/nvim-lspconfig"] = {
+		config = function()
+			require("plugins.configs.lspconfig")
+			require("custom.override.lspconfig")
+		end,
+	},
+	["jose-elias-alvarez/null-ls.nvim"] = {
+		after = "nvim-lspconfig",
+		config = function()
+			require("custom.plugins.null_ls").setup()
+		end,
+	},
+	["folke/todo-comments.nvim"] = {
+		config = function()
+			require("todo-comments").setup()
+		end,
+		event = "BufRead",
+	},
+	["simrat39/symbols-outline.nvim"] = {
+		event = "BufReadPost",
+		config = function()
+			require("symbols-outline").setup()
+		end,
+	},
+	["j-hui/fidget.nvim"] = {
+		config = function()
+			require("fidget").setup({})
+		end,
+	},
+	["b0o/schemastore.nvim"] = {},
+	["ahmedkhalf/project.nvim"] = {
+		config = function()
+			require("project_nvim").setup()
+		end,
+	},
+	["andrewferrier/debugprint.nvim"] = {
+		config = function()
+			require("debugprint").setup()
+		end,
 	},
 }
 

82edf2b85a158d64ffa07db8af89687c9075557d
neo-f <tianpengfei@moonshot.cn>
2024-08-02 17:50:37 +0800

    update nvim & tmux

--- .config/nvim/lua/config/lazy.lua
+++ .config/nvim/lua/config/lazy.lua
@@ -17,7 +17,7 @@ require("lazy").setup({
     {
       "LazyVim/LazyVim",
       import = "lazyvim.plugins",
-      opts = { colorscheme = "catppuccin" },
+      opts = { colorscheme = "tokyonight" },
     },
 
     -- import/override with your plugins
